430100aa837eb30fbf10ef75ddb15ad364b7208e
Paul Smith <paulsmithkc@gmail.com>
2019-02-23 16:09:58 -0600

    Refactoring the GradeUploadController to remove duplicates and inform the users of errors.

---
+++ CodeForACause/StlCollegePrepWebsite/Models/GradeUploadViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace StlCollegePrepWebsite.Models
+{
+    public class GradeUploadViewModel
+    {
+        [Required]
+        public int? Year { get; set; }
+        [Required]
+        public string Semester { get; set; }
+        [Required]
+        public HttpPostedFileBase PostedFile { get; set; }
+    }
+}
