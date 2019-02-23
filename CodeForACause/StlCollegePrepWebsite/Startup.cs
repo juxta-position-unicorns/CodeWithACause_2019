5029f3ab9e63891827809e95c6295e4f40eec502
benjamin_emnett <benjamin_emnett@insideranken.org>
2019-02-22 21:14:53 -0600

    Added database project

---
+++ CodeForACause/StlCollegePrepWebsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StlCollegePrepWebsite.Startup))]
+namespace StlCollegePrepWebsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
