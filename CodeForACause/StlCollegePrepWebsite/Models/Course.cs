619fe55513916e386931790bbdae4f5be6197302
Geoffrey Gibson <geoffrey.gibson@insideranken.org>
2019-02-22 21:49:40 -0600

    Added model classes: Student, Course, CourseStudent.

---
+++ CodeForACause/StlCollegePrepWebsite/Models/Course.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StlCollegePrepWebsite.Models
+{
+    [Table("Course")]
+    public class Course
+    {
+        [Key]
+        public int CourseId { get; set; }
+
+        [ForeignKey("InstructorId")]
+        public int InstructorId { get; set; }
+
+        public string Subject { get; set; }
+        public int SubjectLevel { get; set; }
+        public double CreditHours { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public int Year { get; set; }
+        public string Semester { get; set; }
+    }
+}
